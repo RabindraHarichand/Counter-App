5b3b0c609a7a470897c4c3214254a6a9c31e54fd
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-02 11:54:36 -0400

    [Section 4]: Create new component

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 
-import { HelloWorldApp } from './HelloWorldApp'; './HelloWorldApp'
+import { HelloWorldApp } from './HelloWorldApp';
+import { FirstApp } from './FirstApp';
+ './HelloWorldApp'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <HelloWorldApp />
+        <FirstApp />
     </React.StrictMode>
 )
