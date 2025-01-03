8e619b8f050b68946cbe1db0a6067b3ee7746cc4
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-02 20:10:16 -0400

    [Section 4]: Add OnClick Event

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,10 +1,18 @@
 import PropTypes from 'prop-types'
 
+
 export const CounterApp = ({value=0}) => {
+    
+    const handleAdd =(event) => {
+        console.log(event)
+    }
+    
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{value}</h2>
+            <h2> { value } </h2>
+
+            <button onClick={handleAdd}>  +1 </button>
         </>
     )
 }
