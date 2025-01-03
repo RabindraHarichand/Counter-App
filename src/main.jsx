34b02543dfa113857aa6f474836d2acd0377a23f
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-02 20:25:39 -0400

    [Section 4]: Add useState Hook

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         {/* <HelloWorldApp />
         <FirstApp /> */}
-        <CounterApp value={123} />
+        <CounterApp value={20} />
     </React.StrictMode>
 )
